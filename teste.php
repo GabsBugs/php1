5ec0515639ec1855593494b4f0f8fcfa038eafc2
Gabrielly <gabriellytiberio@gmail.com>
2023-04-14 18:14:07 -0300

    php

---
+++ teste.php
@@ -0,0 +1,9 @@
+<?php echo "Boas-vindas ao universo do PHP!";
+
+$idade = 22;
+$idade = 21;
+
+echo $idade;
+$idadeDaqui10Anos = $idade + 10;
+echo $idadeDaqui10Anos;
+
---
+++ tipos.php
@@ -0,0 +1,19 @@
+<?php
+$idade = 22;
+
+echo gettype($idade);
+
+$salario = 1000.30;
+echo gettype($salario);
+
+$divisao = 10/3;
+echo gettype($divisao);
+
+$texto = "Olá mundo!";
+echo gettype($texto);
+
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($verdadeiro);
+echo gettype($falso);
